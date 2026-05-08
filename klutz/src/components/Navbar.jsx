121ce88073aed61074213e172414a9eb7a4734f0
Chinmaya-Meher <chinmaya.meher.bca27@tha.edu.in>
2026-05-08 15:28:45 +0530

    fixed navbar responsive layout

--- klutz/src/components/Navbar.jsx
+++ klutz/src/components/Navbar.jsx
@@ -2,26 +2,33 @@
 
 import "./Navbar.css";
 
+import { useState } from "react";
+
 import {
-  FiUser,
-  FiShoppingCart,
   FiMoon,
+  FiMenu,
+  FiX,
 } from "react-icons/fi";
 
 export default function Navbar({
   darkMode,
   setDarkMode,
 }) {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="navbar-wrapper">
 
       <nav className="navbar">
 
+        {/* LOGO */}
+
         <div className="logo">
           <h1>KLTUZ.</h1>
         </div>
 
-        {/* LINKS */}
+        {/* DESKTOP LINKS */}
 
         <div className="nav-links">
 
@@ -47,9 +54,11 @@ export default function Navbar({
 
         </div>
 
-        {/* ICONS */}
+        {/* RIGHT SIDE */}
 
         <div className="nav-icons">
+
+          {/* DARK MODE */}
 
           <FiMoon
             className="theme-icon"
@@ -58,10 +67,52 @@ export default function Navbar({
             }
           />
 
+          {/* HAMBURGER */}
+
+          <div
+            className="hamburger"
+            onClick={() =>
+              setMenuOpen(!menuOpen)
+            }
+          >
+
+            {
+              menuOpen
+                ? <FiX />
+                : <FiMenu />
+            }
+
+          </div>
 
         </div>
 
       </nav>
+
+      {/* MOBILE MENU */}
+
+      <div className={`mobile-menu ${menuOpen ? "active" : ""}`}>
+
+        <a href="#home">
+          Home
+        </a>
+
+        <a href="#products">
+          Products
+        </a>
+
+        <a href="#features">
+          Features
+        </a>
+
+        <a href="#contact">
+          Contact Us
+        </a>
+
+        <a href="#faq">
+          FAQs
+        </a>
+
+      </div>
 
     </header>
   );
